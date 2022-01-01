0cc81a90a5acb5c40f3b114142fb77529a08dcf8
Vladis Markin <42187212+Vl4d1s@users.noreply.github.com>
2022-01-01 16:52:11 +0200

    Update 0001-two-sum.js

--- Problems/0001-two-sum/0001-two-sum.js
+++ Problems/0001-two-sum/0001-two-sum.js
@@ -103,3 +103,10 @@ const containsDuplicate = (nums) => {
   }
   return false;
 };
+
+
+// 189. Rotate Array
+function shift(arr, direction, n) {
+  var times = n > arr.length ? n % arr.length : n;
+  return arr.concat(arr.splice(0, direction > 0 ? arr.length - times : times));
+}
